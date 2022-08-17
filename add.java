1ff6677e2da4fbf8a3ac1ba07af51b03c83536da
Sanjay <sanjay27042001@gmail.com>
2022-08-17 16:39:45 +0530

    a chages to 20

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 class First {
     public static void main(String[] args) {
-       int a = 30, b = 10, c = a + b;
-       System.out.println(c);
+        int a = 20, b = 10, c = a + b;
+        System.out.println(c);
 
     }
 }
